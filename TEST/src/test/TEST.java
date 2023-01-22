8fe44cfe6cf76caf5b461e7863441b632086bb0b
Xeniorita Alondra Bio <Xenioritaalondrabio@gmail.com>
2023-01-22 21:03:14 +0800

    BATTLESHIP GAME

---
+++ TEST/src/test/TEST.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import javax.swing.JOptionPane;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+/**
+ *
+ * @author genes
+ */
+public class TEST {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame x = new NewJFrame();
+        x.setVisible(true);
+        
+        
+     
+        
+    }
+       
+}
